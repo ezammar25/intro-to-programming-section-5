73bcec7d0566709fdafa7df5d1a1530019345172
ezammar25 <108433090+ezammar25@users.noreply.github.com>
2022-09-25 19:15:46 -0400

    updated section 5

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const tooLowMessage = document.getElementById('too-low');
 const maxGuessesMessage = document.getElementById('max-guesses');
 const numberOfGuessesMessage = document.getElementById('num-of-guesses');
 const correctMessage = document.getElementById('correct');
-const rangeCheck = document.getElementById('range-check');
+// const rangeCheck = document.getElementById('range-check');
 
 let targetNumber;
 let attempts = 0;
@@ -19,6 +19,7 @@ let maxNumberOfAttempts = 5;
 // <- 32
 // > getRandomNumber(1, 50)
 // <- 11
+
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -26,27 +27,28 @@ function getRandomNumber(min, max) {
 function checkGuess() {
   // Get value from guess input element
   const guess = parseInt(guessInput.value, 10);
+  attempts = attempts++;
   if (guess < 1) {
-    hideAllMessages(); 
-      resetButton.style.display = 'none';
-      rangeCheck.style.display = '';
-      rangeCheck.innerHTML = "Must be greater than 0";
-      return;
+    hideAllMessages();
+      resetButton.style.display = '';
+      //rangeCheck.style.display = '';
+        tooLowMessage.innerHTML = 'You guessed too low. Try again.';
+        // return;
     }
     if (guess > 99) {
-      resetButton.style.display = 'none';
-      rangeCheck.style.display = '';
-      rangeCheck.innerHTML = "Must be less than 100";
-      return;
+      resetButton.style.display = '';
+      //rangeCheck.style.display = '';
+        tooHighMessage.innerHTML = 'You guessed too high. Try again.';
+        // return;
     } else {
-      resetButton.style.display = 'none';
-      rangeCheck.style.display = 'none';
-      attempts = attempts++;
+      resetButton.style.display = '';
+      guessInput.value = '';
+      //rangeCheck.style.display = 'none';
     }
   console.log(guess);
 }
 
-    hideAllMessages();
+    // hideAllMessages();
   
 
   // if (guess < 1) {
@@ -69,42 +71,50 @@ function checkGuess() {
     numberOfGuessesMessage.style.display = '';
     numberOfGuessesMessage.innerHTML = `You made ${attempts} guesses`;
 
-    correctMessage.style.display = '';
+    correctMessage.style.display = 'Congratulations, You guessed correctly! Would you like to play again?';
 
     submitButton.disabled = true;
     guessInput.disabled = true;
   }  
 
+  const remainingAttempts = maxNumberOfAttempts - attempts;
   if (guess < targetNumber) {
      //if (guess < targetNumber) {
-      tooLowMessage.style.display = '';
+      tooLowMessage.style.display = 'You guessed too low. Try again.';
     } else if (guess > targetNumber) {
-      tooHighMessage.style.display = '';
+      tooHighMessage.style.display = 'You guessed too high. Try again.';
     } else if (guess === targetNumber) {
-      correctMessage.style.display = '';
+      correctMessage.style.display = 'Congratulations, You guessed correctly! Would you like to play again?';
+    } else if (attempts === maxNumberOfAttempts) {
+      submitButton.disabled = true;
+      guessInput.disabled = true;
+      resetButton.disabled = false;
+      maxGuessesMessage.style.display = 'You reached the max number of guesses.';
     }
 
-    const remainingAttempts = maxNumberOfAttempts - attempts;
+    // const remainingAttempts = maxNumberOfAttempts - attempts;
 
     // numberOfGuessesMessage.style.display = '';
     // numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
-    if (remainingAttempts === 1) {
-      numberOfGuessesMessage.style.display = '';
-      numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guess remaining`;
-    } else {
-      numberOfGuessesMessage.style.display = '';
-      numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
-    }
+    // if (remainingAttempts === 1) {
+    //   // numberOfGuessesMessage.style.display = '';
+    //   // numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guess remaining`;
+    // } else {
+    //   // numberOfGuessesMessage.style.display = '';
+    //   // numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
+    // }
   
 
-  if (attempts === maxNumberOfAttempts) {
-    submitButton.disabled = true;
-    guessInput.disabled = true;
-  }
+  // if (attempts === maxNumberOfAttempts) {
+  //   submitButton.disabled = true;
+  //   guessInput.disabled = true;
+  //   resetButton.disabled = false;
+  //   maxGuessesMessage.style.display = 'You reached the max number of guesses.';
+  // }
 
-  guessInput.value = '';
+  // guessInput.value = '';
 
-  resetButton.style.display = '';
+  // resetButton.style.display = '';
   
 
 function hideAllMessages() {
@@ -116,6 +126,7 @@ function hideAllMessages() {
 function setup() {
   // Get random number
   targetNumber = getRandomNumber(1, 100);
+  // resetButton.disabled = true;
   console.log(`target number: ${targetNumber}`);
 
   // Reset number of attempts
